a9048e4b0b3a82a7e7190e52e326ece3f399a516
Hwannam <kyem0910@gmail.com>
2020-06-14 00:59:57 +0900

    윈도우 종료 방지

---
+++ programming/ch1/wk2.cpp
@@ -0,0 +1,10 @@
+#include "iostream"
+using namespace std;
+inline void keep_window_open() { char ch; cin >> ch; } // 윈도우가 닫히는걸 방지함
+
+int main()
+{
+	cout << "Hello Hwan!\n"; //Hello Hwan 출력
+
+	return 0;
+}
